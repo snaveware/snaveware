d2aace3a2555a7c21a41634fa4496c628e8ea20a
snaveware <work.evans020@gmail.com>
2023-07-09 16:21:11 +0300

    added projects, changed stats

--- components/extra-components/Stats.js
+++ components/extra-components/Stats.js
@@ -3,81 +3,81 @@ import StyledStats from "../../styled-components/styled-extra-components/Stats.s
 import { useIsVisible } from "../../Hooks";
 
 const Stats = () => {
-    const containerRef = useRef();
-    const clientsElement = useRef();
-    const projectsElement = useRef();
-    const yearsElement = useRef();
+  const containerRef = useRef();
+  const clientsElement = useRef();
+  const projectsElement = useRef();
+  const yearsElement = useRef();
 
-    const isVisible = useIsVisible(containerRef);
+  const isVisible = useIsVisible(containerRef);
 
-    useEffect(() => {
-        for (let index = 1; index <= 8; index++) {
-            setTimeout(() => {
-                clientsElement.current.innerText = `${index}`;
-            }, index * 200);
-        }
+  useEffect(() => {
+    for (let index = 1; index <= 12; index++) {
+      setTimeout(() => {
+        clientsElement.current.innerText = `${index}`;
+      }, index * 200);
+    }
 
-        for (let index = 1; index <= 12; index++) {
-            setTimeout(() => {
-                projectsElement.current.innerText = `${index}`;
-            }, index * 100);
-        }
+    for (let index = 1; index <= 20; index++) {
+      setTimeout(() => {
+        projectsElement.current.innerText = `${index}`;
+      }, index * 100);
+    }
 
-        for (let index = 1; index <= 4; index++) {
-            setTimeout(() => {
-                yearsElement.current.innerText = `${index}`;
-            }, index * 200);
-        }
-    }, [isVisible]);
+    for (let index = 1; index <= 4; index++) {
+      setTimeout(() => {
+        yearsElement.current.innerText = `${index}`;
+      }, index * 200);
+    }
+  }, [isVisible]);
 
-    return (
-        <StyledStats ref={containerRef}>
-            <div className="stats-item stats-clients-container">
-                <div className="stats-value-container">
-                    <div className="stats-value-container-inner">
-                        <p>
-                            <span className="stats-value" ref={clientsElement}>
-                                8
-                            </span>
-                            <span className="stats-value">{" + "}</span>
-                        </p>
-                    </div>
-                </div>
+  return (
+    <StyledStats ref={containerRef}>
+      <div className="stats-item stats-clients-container">
+        <div className="stats-value-container">
+          <div className="stats-value-container-inner">
+            <p>
+              <span className="stats-value" ref={clientsElement}>
+                12
+              </span>
+              <span className="stats-value">{" + "}</span>
+            </p>
+          </div>
+        </div>
 
-                <p className="stats-label">Happy Clients</p>
-            </div>
+        <p className="stats-label">Happy Clients</p>
+      </div>
 
-            <div className="stats-item stats-projects-container">
-                <div className="stats-value-container">
-                    <div className="stats-value-container-inner">
-                        <p>
-                            <span className="stats-value" ref={projectsElement}>
-                                12
-                            </span>
-                            <span className="stats-value">{" + "}</span>
-                        </p>
-                    </div>
-                </div>
+      <div className="stats-item stats-projects-container">
+        <div className="stats-value-container">
+          <div className="stats-value-container-inner">
+            <p>
+              <span className="stats-value" ref={projectsElement}>
+                20
+              </span>
+              <span className="stats-value">{" + "}</span>
+            </p>
+          </div>
+        </div>
 
-                <p className="stats-label">Competed Projects</p>
-            </div>
+        <p className="stats-label">Competed Projects</p>
+      </div>
 
-            <div className="stats-item stats-years-container">
-                <div className="stats-value-container">
-                    <div className="stats-value-container-inner">
-                        <p>
-                            <span className="stats-value" ref={yearsElement}>
-                                4
-                            </span>
-                            <span className="stats-value">{" + "}</span>
-                        </p>
-                    </div>
-                </div>
+      <div className="stats-item stats-years-container">
+        <div className="stats-value-container">
+          <div className="stats-value-container-inner">
+            <p>
+              <span className="stats-value" ref={yearsElement}>
+                4
+              </span>
+              <span className="stats-value">{" + "}</span>
+            </p>
+          </div>
+        </div>
 
-                <p className="stats-label">Years of Excellence</p>
-            </div>
-        </StyledStats>
-    );
+        <p className="stats-label">Years of Excellence</p>
+      </div>
+    </StyledStats>
+  );
 };
 
 export default Stats;
--- data/ProjectsData.js
+++ data/ProjectsData.js
@@ -1,84 +1,114 @@
 const ProjectsData = [
-    {
-        name: "Global",
-        imageUrl: {
-            webp: "/projects/global.webp",
-            other: "/projects/global.jpg",
-        },
-        description: "A Shipping Management System",
-        btnText: "Live System",
-        btnLink: "https://global.snaveware.com",
+  {
+    name: "Global",
+    imageUrl: {
+      webp: "/projects/ledja.webp",
+      other: "/projects/ledja.jpg",
     },
-    {
-        name: "Anchorbay",
-        imageUrl: {
-            webp: "/projects/anchorbay.webp",
-            other: "/projects/anchorbay.jpg",
-        },
-        description: "A Media Production Company Website",
-        btnText: "Live Website",
-        btnLink: "https://anchorbayproductions.com",
+    description: "An online Job Board Webapp",
+    btnText: "Live App Demo",
+    btnLink: "https://ledja.snaveware.com",
+  },
+  {
+    name: "Global",
+    imageUrl: {
+      webp: "/projects/global.webp",
+      other: "/projects/global.jpg",
     },
-    {
-        name: "Midori",
-        imageUrl: {
-            webp: "/projects/midori.webp",
-            other: "/projects/midori.jpg",
-        },
-        description: "A Wellness Essentials E-Shop",
-        btnText: "Live Website",
-        btnLink: "https://eshop.midori.ke",
+    description: "A Shipping Management System",
+    btnText: "Live System Demo",
+    btnLink: "https://global.snaveware.com",
+  },
+  {
+    name: "ONM Law",
+    imageUrl: {
+      webp: "/projects/onmlaw.webp",
+      other: "/projects/onmlaw.jpg",
     },
-    {
-        name: "Aroma",
-        imageUrl: {
-            webp: "/projects/aroma.webp",
-            other: "/projects/aroma.jpg",
-        },
-        description: "A Hotel Website Template",
-        btnText: "Live Demo",
-        btnLink: "https://aroma.snaveware.com",
+    description: "A Law Firm Website",
+    btnText: "Live Website",
+    btnLink: "https://onmlaw.com",
+  },
+  {
+    name: "Anchorbay",
+    imageUrl: {
+      webp: "/projects/anchorbay.webp",
+      other: "/projects/anchorbay.jpg",
     },
-    {
-        name: "Fitness",
-        imageUrl: {
-            webp: "/projects/fitness.webp",
-            other: "/projects/fitness.jpg",
-        },
-        description: "A Fitness Website Template",
-        btnText: "Live Demo",
-        btnLink: "https://fitness.snaveware.com",
+    description: "A Media Production Company Website",
+    btnText: "Live Website",
+    btnLink: "https://anchorbayproductions.com",
+  },
+  {
+    name: "Midori",
+    imageUrl: {
+      webp: "/projects/midori.webp",
+      other: "/projects/midori.jpg",
     },
-    {
-        name: "Metameta",
-        imageUrl: {
-            webp: "/projects/metameta.webp",
-            other: "/projects/metameta.jpg",
-        },
-        description: "A School Website",
-        btnText: "Live Website",
-        btnLink: null,
+    description: "A Wellness Essentials E-Shop",
+    btnText: "Live Website",
+    btnLink: null,
+  },
+  {
+    name: "Aroma",
+    imageUrl: {
+      webp: "/projects/aroma.webp",
+      other: "/projects/aroma.jpg",
     },
-    {
-        name: "Borrower's Paradise",
-        imageUrl: {
-            webp: "/projects/BP.webp",
-            other: "/projects/BP.jpg",
-        },
-        description: "A Loan Management mobile application",
-        btnText: "Live app",
-        btnLink: null,
+    description: "A Hotel Website Template",
+    btnText: "Live Demo",
+    btnLink: "https://aroma.snaveware.com",
+  },
+  {
+    name: "Fitness",
+    imageUrl: {
+      webp: "/projects/fitness.webp",
+      other: "/projects/fitness.jpg",
     },
-    {
-        name: "Kenanie Gardens",
-        imageUrl: {
-            webp: "/projects/kenanie.webp",
-            other: "/projects/kenanie.jpg",
-        },
-        description: "A property Advertising Website",
-        btnText: "Live website",
-        btnLink: "https://www.kenaniegardens.com/",
+    description: "A Fitness Website Template",
+    btnText: "Live Demo",
+    btnLink: null,
+  },
+  {
+    name: "Metameta",
+    imageUrl: {
+      webp: "/projects/metameta.webp",
+      other: "/projects/metameta.jpg",
     },
+    description: "A School Website",
+    btnText: "Live Website",
+    btnLink: null,
+  },
+  {
+    name: "Borrower's Paradise",
+    imageUrl: {
+      webp: "/projects/BP.webp",
+      other: "/projects/BP.jpg",
+    },
+    description: "A Loan Management mobile application",
+    btnText: "Live Demo",
+    btnLink: null,
+  },
+  {
+    name: "TUK Projects Repository",
+    imageUrl: {
+      webp: "/projects/tpr.webp",
+      other: "/projects/tpr.jpg",
+    },
+    description: "A University Projects Repository",
+    btnText: "Live Demo",
+    btnLink: "https://tpr.snaveware.com/",
+  },
+  {
+    name: "Kenanie Gardens",
+    imageUrl: {
+      webp: "/projects/kenanie.webp",
+      other: "/projects/kenanie.jpg",
+    },
+    description: "A property Advertising Website",
+    btnText: "Live website",
+    btnLink: "https://www.kenaniegardens.com/",
+  },
 ];
 
 export default ProjectsData;
