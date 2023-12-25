e16df23b828253484ac08ffb4ad63fa2eb545f56
snaveware <work.evans020@gmail.com>
2023-12-25 10:44:00 +0300

    added google script

--- pages/_app.js
+++ pages/_app.js
@@ -7,51 +7,65 @@ import Scrollbar from "smooth-scrollbar";
 // import { Scrollbar } from "smooth-scrollbar-react";
 
 const MyApp = ({ Component, pageProps }) => {
-    const scrollOptions = {
-        damping: 0.1,
-        thumbMinSize: 50,
-        renderByPixels: true,
-        alwaysShowTracks: true,
-        continuousScrolling: true,
-    };
+  const scrollOptions = {
+    damping: 0.1,
+    thumbMinSize: 50,
+    renderByPixels: true,
+    alwaysShowTracks: true,
+    continuousScrolling: true,
+  };
 
-    useEffect(() => {
-        const darkmode = localStorage.getItem("dark_mode");
+  useEffect(() => {
+    const darkmode = localStorage.getItem("dark_mode");
 
-        if (darkmode == 1) {
-            setTheme(Themes.dark);
+    if (darkmode == 1) {
+      setTheme(Themes.dark);
+    } else {
+      if (window.matchMedia) {
+        // Check if the dark-mode Media-Query matches
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+          setTheme(Themes.dark);
         } else {
-            if (window.matchMedia) {
-                // Check if the dark-mode Media-Query matches
-                if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-                    setTheme(Themes.dark);
-                } else {
-                    setTheme(Themes.light);
-                }
-            }
+          setTheme(Themes.light);
         }
+      }
+    }
 
-        // Scrollbar.init(document.body, scrollOptions);
-    }, []);
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-K5ZXVF55Z0";
+    script1.async = true;
+    document.body.appendChild(script1);
 
-    const [theme, setTheme] = useState(Themes.default);
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K5ZXVF55Z0');
+        `;
+    document.body.appendChild(script2);
 
-    return (
-        <ThemeProvider theme={{ ...theme, setTheme }}>
-            <GlobalStyles />
-            {/* <Scrollbar
+    // Scrollbar.init(document.body, scrollOptions);
+  }, []);
+
+  const [theme, setTheme] = useState(Themes.default);
+
+  return (
+    <ThemeProvider theme={{ ...theme, setTheme }}>
+      <GlobalStyles />
+      {/* <Scrollbar
                 damping={0.1}
                 thumbMinSize={20}
                 renderByPixels={true}
                 alwaysShowTracks={true}
                 continuousScrolling={true}
             > */}
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-            {/* </Scrollbar> */}
-        </ThemeProvider>
-    );
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      {/* </Scrollbar> */}
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
